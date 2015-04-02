14346b5182785cf73792446adeee78f8b126b235
jdhakar <jnagar1729@gmail.com>
2015-04-02 13:06:25 +0530

    edited mutable draw

--- GraphDraw/MutableDraw.hs
+++ GraphDraw/MutableDraw.hs
@@ -30,13 +30,13 @@ tupleListRand n = out
 
 giveVec :: (DynGraph gr) => gr a b -> Int -> V.Vector Location
 giveVec graph n = runST $ do
-	position  <- V.thaw pos1
-	force     <- V.thaw for1
-	updateGraph graph n position force
- 	V.freeze position
- 	where
- 		pos1 = V.fromList (tupleListRand (noNodes  graph))
- 		for1 = V.fromList (tupleListZero (noNodes  graph))
+        position  <- V.thaw pos1
+        force     <- V.thaw for1
+        updateGraph graph n position force
+         V.freeze position
+         where
+                 pos1 = V.fromList (tupleListRand (noNodes  graph))
+                 for1 = V.fromList (tupleListZero (noNodes  graph))
 
 updateGraph :: (PrimMonad m , DynGraph gr) => gr c d -> Int -> 
               M.MVector (PrimState m) Location -> 
@@ -51,21 +51,20 @@ updateGraph inGraph n pos force  = do
       M.write force (i-1) (0.0,0.0)
 
 forceAtN :: ( DynGraph gr , PrimMonad m) => gr c d -> M.MVector (PrimState m) Location ->  
-			M.MVector (PrimState m) Force -> m ()
+                        M.MVector (PrimState m) Force -> m ()
 forceAtN g pos force = do
-	forM_ [1..noNodes g] $ \n -> do
---		M.write force (n-1) (0.0,0.0)
-		forM_ (neighbors g n) $ \i -> do
-			attForce pos force n i 2
-		forM_ ((nodes g) \\ ((neighbors g n)++[n])) $ \j -> do
-			attForce pos force j n 1
-
+        forM_ [1..noNodes g] $ \n -> do
+--                M.write force (n-1) (0.0,0.0)
+                forM_ (neighbors g n) $ \i -> do
+                        attForce pos force n i 2
+                forM_ ((nodes g) \\ ((neighbors g n)++[n])) $ \j -> do
+                        attForce pos force j n 1
 
 tupleAdd :: (Double,Double) -> (Double,Double) -> (Double,Double)
 tupleAdd (x1,y1) (x2,y2) = (x1+x2,y1+y2)
         
 attForce :: (PrimMonad m)=> M.MVector (PrimState m) Location -> M.MVector (PrimState m) Force -> 
-			Int -> Int -> Int -> m ()
+                        Int -> Int -> Int -> m ()
 attForce pos force n1 n2 typeOf = do
   pos1 <- M.read pos (n1-1)
   pos2 <- M.read pos (n2-1)
